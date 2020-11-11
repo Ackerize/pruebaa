5af0699d242d503fbd5975cd367863cb0930577b
David Vallecios <37277365+DavidVallecios@users.noreply.github.com>
2020-11-11 14:28:58 -0600

    Update auxiliar.cpp

--- auxiliar.cpp
+++ auxiliar.cpp
@@ -7,28 +7,6 @@ int rows = 7;
 int cols = 7;
 
 
-/*  Variables globales:
-    rows = cantidad de filas de la matriz 
-    cols = cantidad de columnas de la matriz
-*/
-void agregarUnoAbajo(int **matriz, string posicion){
-    int currentRow = atoi(&posicion[0]); 
-    
-    int currentCol = atoi(&posicion[2]); 
-
-
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < cols; j++){
-            if(i == currentRow && j == currentCol){
-                matriz[i][j] = 1;
-                if(i > 0){
-                    matriz[i+1][j] = 1;
-                }
-            }
-        }
-    }
-}
-
 void imprimirArreglo(int **matriz){
     for(int i=0;i< rows;i++){
         for(int j=0;j< cols;j++){
